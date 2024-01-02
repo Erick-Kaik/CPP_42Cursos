e279638180778a66b85c26874c497606464ff9c3
Erick-Kaik <erick.kaik2002@gmail.com>
2024-01-02 16:12:33 -0300

    upd CPP_02

---
+++ CPP_02/ex00/src/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ekaik-ne <ekaik-ne@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/29 15:56:28 by ekaik-ne          #+#    #+#             */
+/*   Updated: 2023/12/29 15:59:14 by ekaik-ne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/Fixed.hpp"
+
+int main(void)
+{
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return (0);
+}
